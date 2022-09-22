67e94efcde9efdf34372a24e3f900f9980b20a01
a4anna <shilpag@andrew.cmu.edu>
2022-09-22 10:02:54 -0400

    Adding bounding box feature in UI

--- home/static/js/main.js
+++ home/static/js/main.js
@@ -1,27 +1,107 @@
 var c = document.getElementById("canvas");
 var ctx = c.getContext("2d");
 var image = new Image();
-console.log(image);
-image.onload = function(e) {
-    ctx.drawImage(image, 0, 0, 256, 256);
+var dimension = 256;
+console.log(`Label changed ${changed}`)
+
+var drawBoxes = function(id, xMin, xMax, yMin, yMax) {
+    ctx.strokeStyle = "red";
+    ctx.fillStyle = "red";
+    ctx.rect(xMin, yMin, xMax - xMin, yMax - yMin);
+    ctx.lineWidth="2";
+    ctx.stroke();
+    ctx.font = "bold 12px Courier";
+    ctx.fillText("Box:" + id, xMin,yMin-2);
 };
+
+image.onload = function(e) {
+    ctx.canvas.width = dimension;
+    ctx.canvas.height = dimension;
+    c.width = dimension;
+    c.height = dimension;
+    ctx.drawImage(image, 0, 0, dimension, dimension);
+    console.log(`Box add ${boxes.length} ${boxes}`)
+    for (i = 0; i < boxes.length; i++){
+        drawBoxes(boxes[i].id, boxes[i].xMin, boxes[i].xMax, boxes[i].yMin, boxes[i].yMax);
+    } 
+};
+
 image.style.display = "block";
 image.src = "image/" + image_path;
 var label = 0; 
+
+var clicked = false;
+var fPoint = {};
+c.onclick = function(e) {
+    console.log(clicked);
+    if (!clicked) {
+        var x = (dimension / c.scrollWidth) * e.offsetX;
+        var y = (dimension / c.scrollHeight) * e.offsetY;
+        console.log(e);
+        ctx.strokeStyle = "red";
+        ctx.fillStyle = "red";
+        ctx.beginPath();
+        ctx.arc(x, y, 3, 0, 2*Math.PI, false);
+        ctx.fill();
+        fPoint = {
+            x: x,
+            y: y
+        };
+    } else {
+        var x = (dimension / c.scrollWidth) * e.offsetX;
+        var y = (dimension / c.scrollHeight) * e.offsetY;
+        var xMin;
+        var xMax;
+        var yMin;
+        var yMin;
+        if (x > fPoint.x) {
+            xMax = x;
+            xMin = fPoint.x;
+        } else {
+            xMax = fPoint.x;
+            xMin = x;
+        }
+        if (y > fPoint.y) {
+            yMax = y;
+            yMin = fPoint.y;
+        } else {
+            yMax = fPoint.y;
+            yMin = y;
+        }
+        fPoint = {};
+        console.log(`Box add`)
+        window.location.replace("/add/" + (boxes.length + 1) +
+        "?xMin=" + xMin +
+        "&xMax=" + xMax +
+        "&yMin=" + yMin +
+        "&yMax=" + yMax);
+        changed = 1;
+    }
+    clicked = !clicked;
+};
+
 console.log(`Key pressed ${color} \n Key code Value: ${label_text}`);
 
 document.getElementById("demo").addEventListener("keypress", (event) => {
+    changed = 0;
     var name = event.key;
     var code = event.code;
     console.log(`Key pressed ${name} \n Key code Value: ${code}`);
-
+    const valid_keys = ['KeyP', 'KeyN', 'KeyU'];
     if (code === 'KeyP') {
-        label = 1; 
+        label = '1'; 
+        changed = 1;
     }
     else if (code === 'KeyN') {
-        label = 0; 
+        label = '0'; 
+        changed = 1;
     }
-    if (code === 'KeyP' || code === 'KeyN') {
+    else if (code === 'KeyU') {
+        label = '-1'; 
+        changed = 0;
+        unsaved = false;
+    }
+    if (valid_keys.includes(code)) {
         window.location.replace("/classify/" + image + "?label=" + label);
         document.getElementById('labelSquare').style.backgroundColor = color;
         document.getElementById("labelText").innerHTML = label_text;
@@ -35,13 +115,53 @@ document.getElementById("demo").addEventListener("keydown", (event) => {
     var name = event.key;
     var code = event.code;
     console.log(`Key pressed ${name} \n Key code Value: ${code}`);
+    console.log(`Label ${changed} `);
 
     if (code === 'ArrowLeft') {
+        if (changed === 1 && save_auto === 0) {
+            unsaved = true
+            window.dispatchEvent(new Event('beforeunload'))
+        }
         window.location.replace("/prev");
         return;
     }
     else if (code === 'ArrowRight'){
+        if (changed === 1 && save_auto === 0) {
+            unsaved = true
+            window.dispatchEvent(new Event('beforeunload'))
+        }
         window.location.replace("/next");
         return;
     }
+    else if (code === 'KeyS'){
+        changed = 0;
+        unsaved = false;
+        window.location.replace("/save");
+        return;
+    }
 }, false);
+
+var timer
+
+window.addEventListener('beforeunload', (event) => {
+  timer = window.setTimeout( function () {
+    unsaved = false;
+  }, 50);
+  // if (unsaved === true) {
+  //   var confirmationMessage = 'Are you sure to leave the page?';  // a space
+  //   (event || window.event).returnValue = confirmationMessage;
+  //   return confirmationMessage;
+  // }
+});
+
+window.addEventListener('beforeunload', (event) => {
+  console.log(`UNSAVED ${unsaved} ${changed}`);
+  if (unsaved === true) {
+    event.preventDefault();
+    event.returnValue = 'Are you sure to leave the page?';
+  }
+});
+
+window.addEventListener("unload", function(event) {
+  window.clearTimeout(timer)
+})
--- home/ui_labeler.py
+++ home/ui_labeler.py
@@ -6,6 +6,7 @@ import imghdr
 import os
 import time
 import multiprocessing as mp
+import numpy as np
 
 from os import walk
 from flask import Flask, Response, redirect, url_for, request
@@ -34,7 +35,7 @@ class EndpointAction(object):
 class UILabeler(object):
     app = None
 
-    def __init__(self, mission_dir):
+    def __init__(self, mission_dir, save_automatically=False):
         self.app = Flask(__name__)
         self.app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
 
@@ -42,6 +43,14 @@ class UILabeler(object):
         self._image_dir = os.path.join(mission_dir, 'images')
         self._meta_dir = os.path.join(mission_dir, 'meta')
         self._label_dir = os.path.join(mission_dir, 'labels')
+        self._label_map = {
+            '-1': {'color': '#ffffff', 
+                    'text': 'UNLABELED'},
+            '0': {'color': '#ae163e',
+                   'text': 'NEGATIVE'},
+            '1': {'color': 'green',
+                  'text': 'POSITIVE'}
+        }
         
         directory = self._image_dir
         if directory[len(directory) - 1] != "/":
@@ -49,18 +58,22 @@ class UILabeler(object):
         self.app.config["IMAGES"] = directory
         self.app.config["LABELS"] = []
         self.app.config["HEAD"] = 0
+        self.label_changed = False
+        self.save_auto = save_automatically
+         
         files = None
-        for (dirpath, dirnames, filenames) in walk(self.app.config["IMAGES"]):
+        for (_, _, filenames) in walk(self.app.config["IMAGES"]):
             files = sorted(filenames)
             break
         if files == None:
             print("No files")
             exit()
-            self.app.config["HEAD"] = -1
         else:
             self.app.config["LABELS"] = ['-1'] * len(files)
         self.app.config["FILES"] = files
+        self.not_end = True 
         
+        self.image_boxes = []
         self.add_all_endpoints()
 
     def start_labeling(self, input_q, result_q, stats_q, stop_event):
@@ -73,7 +86,7 @@ class UILabeler(object):
         self.bytes = 0
 
         try:
-            self.app.run(use_reloader=False)
+            self.app.run(port=8000, use_reloader=False)
         except KeyboardInterrupt as e:
             raise e 
         
@@ -83,13 +96,19 @@ class UILabeler(object):
         self.positives = 0 
         self.negatives = 0 
         self.bytes = 0
-        self.app.run()
+        self.app.jinja_env.filters['bool'] = bool
+        self.app.run(port=8000)
+
         
     def add_all_endpoints(self):
-        # Add root endpoint
+        # Add endpoints
         self.add_endpoint(endpoint='/', endpoint_name='index', handler=self.index)
         self.add_endpoint(endpoint='/next', endpoint_name='next', handler=self.next)
         self.add_endpoint(endpoint='/prev', endpoint_name='prev', handler=self.prev)
+        self.add_endpoint(endpoint='/save', endpoint_name='save', handler=self.save)
+        self.add_endpoint(endpoint='/undo', endpoint_name='undo', handler=self.undo)
+        self.add_endpoint(endpoint='/add/<id>', endpoint_name='add', handler=self.add)
+        self.add_endpoint(endpoint='/remove/<id>', endpoint_name='remove', handler=self.remove)
         self.add_endpoint(endpoint='/image/<f>', endpoint_name='images', handler=self.images)
         self.add_endpoint(endpoint='/classify/<id>', endpoint_name='classify', handler=self.classify)
         
@@ -98,40 +117,42 @@ class UILabeler(object):
         # You can also add options here : "... , methods=['POST'], ... "
 
     # ==================== ------ API Calls ------- ====================
-    def index(self, *args, **kwargs) :
-        label_map = {
-            '-1': {'color': '#ffffff', 
-                    'text': 'UNLABELED'},
-            '0': {'color': '#ae163e',
-                   'text': 'NEGATIVE'},
-            '1': {'color': 'green',
-                  'text': 'POSITIVE'}
-        }
+    def index(self, *args, **kwargs):
         self.reload_directory()
         if (len(self.app.config["FILES"]) == 0):
+            self.not_end = False
             self.app.config["HEAD"] = -1 
-            return render_template('index.html', not_end=True, 
+            return render_template('index.html', 
                                    directory="", image="", 
                                    head=0, 
                                    len=len(self.app.config["FILES"]),
-                                   color='#ffffff', label_text="")
+                                   color='#ffffff', label_text="", 
+                                   boxes=self.image_boxes,
+                                   label_changed= 1 if self.label_changed else 0)
         
-        if (self.app.config["HEAD"] == len(self.app.config["FILES"])):
-            logger.info("Waiting for Results ...")
-            while (self.app.config["HEAD"] == len(self.app.config["FILES"])):
-                time.sleep(1)
-                self.reload_directory()
         directory = self.app.config['IMAGES']
-        image = self.app.config["FILES"][self.app.config["HEAD"]]
-        label = self.app.config["LABELS"][self.app.config["HEAD"]]
-        color = label_map[label]['color']
-        label_text = label_map[label]['text']
-        not_end = not(self.app.config["HEAD"] == len(self.app.config["FILES"]) - 1)
-        return render_template('index.html', not_end=not_end, 
-                               directory=directory, image=image, 
+        image_path = self.app.config["FILES"][self.app.config["HEAD"]]
+
+        if (self.app.config["HEAD"] == 0 and not self.label_changed):
+            label, boxes = self.read_labels()
+            self.app.config["LABELS"][self.app.config["HEAD"]] = label
+            self.image_boxes = boxes
+        else:
+            label = self.app.config["LABELS"][self.app.config["HEAD"]]
+            
+        color = self._label_map[label]['color']
+        label_text = self._label_map[label]['text']
+        self.not_end = not(self.app.config["HEAD"] == len(self.app.config["FILES"]) - 1)
+        condition_changed = self.label_changed and not self.save_auto
+
+        return render_template('index.html', 
+                               directory=directory, image=image_path, 
                                head=self.app.config["HEAD"] + 1, 
-                               len=len(self.app.config["FILES"]),
-                               color=color, label_text=label_text)
+                               files=len(self.app.config["FILES"]),
+                               color=color, label_text=label_text,
+                               boxes=self.image_boxes,
+                               label_changed=int(condition_changed == True),
+                               save=int(self.save_auto == True))
 
     def reload_directory(self):
         old_length = len(self.app.config["FILES"])
@@ -144,16 +165,153 @@ class UILabeler(object):
         self.app.config["FILES"] = files
         new_files = len(self.app.config["FILES"]) - old_length
         [self.app.config["LABELS"].append('-1') for i in range(new_files)]
+        self.not_end = not(self.app.config["HEAD"] == len(self.app.config["FILES"]) - 1)
+        if new_files and self.app.config["HEAD"] < 0:
+            self.app.config["HEAD"] = 0
         return
 
+    def read_labels(self):
+        path = self.app.config["FILES"][self.app.config["HEAD"]]
+        data_name = os.path.splitext(path)[0]
+        label_path = os.path.join(self._label_dir, f"{data_name}.json")
+
+        if os.path.exists(label_path):
+            with open(label_path, "r") as f:
+                image_labels = json.load(f)
+                
+            label = str(image_labels['imageLabel'])
+            assert label in ['1', '0'], f"Unkonown label {label}"
+            image_boxes = image_labels['boundingBoxes']
+            boxes = []
+            for i, box in enumerate(image_boxes):
+                _, xMin, xMax, yMin, yMax = box.split(" ")
+                id = i + 1
+                boxes.append({"id":id, 
+                              "xMin":xMin, 
+                              "xMax":xMax, 
+                              "yMin":yMin, 
+                              "yMax":yMax})
+            
+            return label, boxes
+        
+        return '-1', [] 
+       
+    
+    def write_labels(self): 
+        if not self.label_changed:
+            return 
+        path = self.app.config["FILES"][self.app.config["HEAD"]]
+        data_name = os.path.splitext(path)[0]
+        meta_path = os.path.join(self._meta_dir, f"{data_name}.json")
+        label_path = os.path.join(self._label_dir, f"{data_name}.json")
+        image_label = self.app.config["LABELS"][self.app.config["HEAD"]] 
+
+        if image_label == '-1':
+            if os.path.exists(label_path):
+                os.remove(label_path)
+
+        if image_label not in ['1', '0']:
+            return
+
+        data = {}
+        with open(meta_path, "r") as f:
+            data = json.load(f)
+        self.bytes += data['size']
+        # assuming binary class hardcoding positive id as 0
+        bounding_boxes = [ ]
+        
+        for box in self.image_boxes:
+            # convert to yolo fomat
+            bounding_boxes.append(" ".join(['0', 
+                                            box['xMin'],
+                                            box['xMax'],
+                                            box['yMin'],
+                                            box['yMax']])
+            )
+        label = {
+            'objectId': data['objectId'],
+            'scoutIndex': data['scoutIndex'],
+            'imageLabel': image_label, 
+            'boundingBoxes': bounding_boxes, 
+        }    
+        with open(label_path, "w") as f:
+            json.dump(label, f, indent=4, sort_keys=True)
+             
+        self.result_q.put(label_path)
+        image_labels = np.array(self.app.config["LABELS"])
+        self.positives = len(np.where(image_labels == '1')[0])
+        self.negatives = len(np.where(image_labels == '0')[0])
+        logger.info("({}, {}) Labeled {}".format(self.positives, self.negatives, data['objectId']))
+        self.stats_q.put((self.positives, self.negatives, self.bytes))
+        self.label_changed = False
+        return redirect(url_for('index'))
+
+    def save(self, *args, **kwargs):
+        self.write_labels()
+        return redirect(url_for('index'))
+
+    def undo(self, *args, **kwargs):
+        self.label_changed = True
+        label = '-1'
+        self.app.config["LABELS"][self.app.config["HEAD"]] = label
+        if label != '1':
+            self.image_boxes = []
+        self.write_labels()
+        self.label_changed = False
+        return redirect(url_for('index'))
+
     def next(self, *args, **kwargs):
-        self.app.config["HEAD"] = self.app.config["HEAD"] + 1
+        if self.save_auto:
+            self.write_labels()
+        if self.not_end:
+            self.app.config["HEAD"] = self.app.config["HEAD"] + 1
+        else:
+            logger.info("Waiting for Results ...")
+            while (not self.not_end):
+                time.sleep(1)
+                self.reload_directory()
+
+        label, boxes = self.read_labels()
+        self.app.config["LABELS"][self.app.config["HEAD"]] = label
+        self.image_boxes = boxes
+        self.label_changed = False
         return redirect(url_for('index'))
 
     def prev(self, *args, **kwargs):
         if (self.app.config["HEAD"] == 0):
             return redirect(url_for('index'))
         self.app.config["HEAD"] = self.app.config["HEAD"] - 1
+        label, boxes = self.read_labels()
+        self.app.config["LABELS"][self.app.config["HEAD"]] = label
+        self.image_boxes = boxes
+        self.label_changed = False
+        return redirect(url_for('index'))
+
+    def add(self, *args, **kwargs):
+        id = kwargs['id']
+        self.label_changed = True
+        self.app.config["LABELS"][self.app.config["HEAD"]] = '1'
+        xMin = request.args.get("xMin")
+        xMax = request.args.get("xMax")
+        yMin = request.args.get("yMin")
+        yMax = request.args.get("yMax")
+        self.image_boxes.append({"id":id, 
+                                    "xMin":xMin, 
+                                    "xMax":xMax, 
+                                    "yMin":yMin, 
+                                    "yMax":yMax})
+        return redirect(url_for('index'))   
+
+    def remove(self, *args, **kwargs):
+        self.label_changed = True
+        id = kwargs['id']
+        index = int(id) - 1
+        print(self.image_boxes)
+        del self.image_boxes[index]
+        for label in self.image_boxes[index:]:
+            label["id"] = str(int(label["id"]) - 1)
+        if not len(self.image_boxes):
+            self.app.config["LABELS"][self.app.config["HEAD"]] = '-1'
         return redirect(url_for('index'))
 
     def images(self, *args, **kwargs):
@@ -162,35 +320,16 @@ class UILabeler(object):
         return send_file(images + f)
 
     def classify(self, *args, **kwargs):
-        path = self.app.config["FILES"][self.app.config["HEAD"]]
+        self.label_changed = True
         label = request.args.get("label")
         self.app.config["LABELS"][self.app.config["HEAD"]] = label
+        if label != '1':
+            self.image_boxes = []
+        if label == '-1':
+            self.undo()
         
-        data_name = os.path.splitext(path)[0]
-        meta_path = os.path.join(self._meta_dir, f"{data_name}.json")
-        label_path = os.path.join(self._label_dir, f"{data_name}.json")
-        data = {}
-        with open(meta_path, "r") as f:
-            data = json.load(f)
-        image_label = label 
-        if image_label == '1':
-            self.positives += 1
-        else:
-            self.negatives += 1
-        self.bytes += data['size']
-        label = {
-            'objectId': data['objectId'],
-            'scoutIndex': data['scoutIndex'],
-            'imageLabel': image_label, 
-            'boundingBoxes': [], 
-        }    
-        with open(label_path, "w") as f:
-            json.dump(label, f) 
-        self.result_q.put(label_path)
-        logger.info("({}, {}) Labeled {}".format(self.positives, self.negatives, data['objectId']))
-        self.stats_q.put((self.positives, self.negatives, self.bytes))
         return redirect(url_for('index'))
-
-# mission = "/home/shilpag/Documents/hawk-data/dota-tennis-court-hawk_20220822-002846"        
+    
+# mission = "/home/shilpag/Documents/hawk-data/dota-tennis_20220919-080839"
 # a = UILabeler(mission)
 # a.run()
