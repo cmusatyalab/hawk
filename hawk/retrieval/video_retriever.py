640236e3bf5c8b61ba08f58cb304780bba137159
Eric Sturzinger <eric.sturzinger@gmail.com>
2022-09-29 16:50:43 -0400

    video stream updated

--- hawk/retrieval/video_retriever.py
+++ hawk/retrieval/video_retriever.py
@@ -12,6 +12,7 @@ import random
 import numpy as np 
 import time
 import threading
+import multiprocessing as mp
 from typing import Iterable, Sized
 from collections import defaultdict
 from PIL import Image
@@ -24,6 +25,7 @@ from hawk.retrieval.retriever import Retriever
 from hawk.retrieval.retriever_stats import RetrieverStats
 from hawk.core.utils import get_server_ids
 from hawk.core.utils import ATTR_GT_LABEL
+from hawk.retrieval.video_parser import produce_video_frames
 
 class VideoRetriever(Retriever):
 
@@ -44,6 +46,12 @@ class VideoRetriever(Retriever):
         self.overlap = 100 if 100 < 0.5 * self.tilesize else 0
         self.slide = 250
         self.video_file_path = dataset.video_path
+        self.frame_producer_queue = mp.Queue(20)
+        #self.video_frame_producer = VideoFrameProducer(self.video_file_path)
+        p = mp.Process(target=produce_video_frames, args=(self.frame_producer_queue,self.video_file_path),
+                       name="Frame Producer")
+        p.start()
+        ##then create new process and start producer function.
         self.tile_width = self._dataset.tile_width
         self.tile_height = self._dataset.tile_height
         self.video_sampling_rate = self._dataset.sampling_rate_fps
@@ -55,19 +63,7 @@ class VideoRetriever(Retriever):
          #   os.mkdir(self.temp_image_dir)
         if not os.path.exists(self.temp_tile_dir):
             os.mkdir(self.temp_tile_dir)  ## create temp directoy on scout to store carved tiles
-        ## bash comamnd to mount the fifthcraig folder
-        #subprocess.run(["ffmpeg", "-i", self.video_file_path, "-vf", "fps=1", os.path.join(self.temp_image_dir, "%04d.png")])
-        mount_dir = r"/srv/diamond/fifthcraig"
-        '''
-        if len(os.listdir(mount_dir)) != 0:
-            unmount_cmd = ["sudo", "fusermount", "-u", "/srv/diamond/fifthcraig"]
-            out = subprocess.check_call(unmount_cmd)  ##unmount the AWS bucket
-        else:
-            mount_cmd_list  = ["sudo", "/usr/bin/s3fs", "hawk-fifthcraig", "/srv/diamond/fifthcraig", "-o", "ro", "-o", "use_path_request_style", "-o", "url=https://storage.cmusatyalab.org"]
-            subprocess.run(mount_cmd_list)
-        logger.info(out)
-        print(out)
-        '''
+
         self._stats['total_objects'] = 0
         self._stats['total_images'] = 0
         self.total_tiles = 192 * len(os.listdir(self.temp_image_dir))
@@ -81,51 +77,31 @@ class VideoRetriever(Retriever):
         cv2.imwrite(outdir, outimg)
         return outdir
 
-    def split_frame(self, frame_name):
-        #with cv2.imread(os.path.join(self.temp_image_dir, frame_name)) as frame:
-        frame = cv2.imread(os.path.join(self.temp_image_dir, frame_name))
+    def split_frame(self, frame_name, frame):
+        logger.info(self.frame_producer_queue.qsize())
+        #frame = cv2.imread(os.path.join(self.temp_image_dir, frame_name))
         outbasename = frame_name.split(".")[0] + '_'
-        width = np.shape(frame)[1]
-        height = np.shape(frame)[0]
+        width = 4000
+        height = 3000
         num_tile_rows = height / self.tilesize
         num_tile_cols = width / self.tilesize
         tiles = []
+        dirname = self.temp_tile_dir
         for row in range(int(num_tile_rows)):
             for col in range(int(num_tile_cols)):
-                subimgname = outbasename + str(col*self.tilesize) + '_' + str(row*self.tilesize)
-                tile = self.save_tile(frame, subimgname, col*self.tilesize, row*self.tilesize)
-                tiles.append(tile)
-        del frame
-        gc.collect()
+                subimgname = outbasename + str(col * self.tilesize) + '_' + str(row * self.tilesize)
+                # tile = self.save_tile(frame, subimgname, col*self.tilesize, row*self.tilesize)
+                tile = frame[row * self.tilesize: (row * self.tilesize + self.tilesize),
+                       col * self.tilesize: (col * self.tilesize + self.tilesize)]
+                # tile = copy.deepcopy(frame[row * self.tilesize: (row * self.tilesize + self.tilesize), col * self.tilesize: (col * self.tilesize + self.tilesize)])
+                outdir = os.path.join(dirname, self.video_file_path.split("/")[-1].split(".")[0] + subimgname + ".jpeg")
+                # h, w, c = np.shape(subimg)
+                tile = cv2.resize(tile, (256, 256))
+                cv2.imwrite(outdir, tile)
+                tiles.append(outdir)
         return tiles
 
 
-        '''
-        left, up = 0, 0
-        tiles = []
-        while (left < weight):
-            if (left + self.tilesize >= weight):
-                left = max(weight - self.tilesize, 0)
-            up = 0
-            while (up < height):
-                if (up + self.tilesize >= height):
-                    up = max(height - self.tilesize, 0)
-                right = min(left + self.tilesize, weight - 1)
-                down = min(up + self.tilesize, height - 1)
-                subimgname = outbasename + str(left) + '_' + str(up)
-                tile = self.save_tile(frame, subimgname, left, up)
-                tiles.append(tile)
-                if (up + self.tilesize >= height):
-                    break
-                else:
-                    up = up + self.slide
-            if (left + self.tilesize >= weight):
-                break
-            else:
-                left = left + self.slide   
-                
-        return tiles
-    '''
     def stream_objects(self):
         # wait for mission context to be added
         while self._context is None:
@@ -135,7 +111,11 @@ class VideoRetriever(Retriever):
         logger.info(self.video_file_path)
         frame_count = 1
         num_retrieved_images = 0
-        for frame_name in os.listdir(self.temp_image_dir):
+        #for frame_name in os.listdir(self.temp_image_dir):
+
+        while not self._stop_event.is_set():
+            logger.info("Waiting for frame from queue...")
+            frame_name, frame = self.frame_producer_queue.get()
             logger.info("Preparing to tile: ")
             if self._stop_event.is_set():
                 logger.info("Stop event is set...")
@@ -144,7 +124,7 @@ class VideoRetriever(Retriever):
             frame_count += 1
             self._stats['retrieved_images'] += 1
             num_retrieved_images += 1
-            tiles = self.split_frame(frame_name)
+            tiles = self.split_frame(frame_name, frame)
 
             logger.info("Retrieved Image: Frame # {} Tiles:{} @ {}".format(
                 num_retrieved_images, len(tiles), time.time() - self._start_time))
@@ -167,6 +147,7 @@ class VideoRetriever(Retriever):
                     ObjectProvider(object_id, content,
                                    DiamondAttributeProvider(attributes, tile_path, resize=False),
                                    int(label)))
+            time.sleep(10)
             logger.info("{} / {} RETRIEVED".format(self._stats['retrieved_tiles'], self.total_tiles))
             time_passed = (time.time() - self._start_time)
             # if time_passed < self.timeout:
