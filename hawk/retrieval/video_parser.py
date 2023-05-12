ac7f77412fd8ed667cf7dc02a6efb993d3a48257
Eric Sturzinger <eric.sturzinger@gmail.com>
2023-05-12 12:23:01 -0400

    Remaining imports removed from video_parser.py

--- hawk/retrieval/video_parser.py
+++ hawk/retrieval/video_parser.py
@@ -18,11 +18,11 @@ from collections import defaultdict
 from PIL import Image
 from logzero import logger
 
-from hawk.core.object_provider import ObjectProvider
-from hawk.proto.messages_pb2 import HawkObject, FileDataset, Streaming_Video
-from hawk.retrieval.retriever import Retriever
-from hawk.retrieval.retriever_stats import RetrieverStats
-from hawk.core.utils import get_server_ids
+#from hawk.core.object_provider import ObjectProvider
+#from hawk.proto.messages_pb2 import HawkObject, FileDataset, Streaming_Video
+#from hawk.retrieval.retriever import Retriever
+#from hawk.retrieval.retriever_stats import RetrieverStats
+#from hawk.core.utils import get_server_ids
 #from hawk.core.utils import ATTR_GT_LABEL
 '''
 class VideoFrameProducer:
