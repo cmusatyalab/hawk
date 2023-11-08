370a62d2d86378905852c06a3254b450e144cf83
Eric Sturzinger <eric.sturzinger@gmail.com>
2023-11-08 14:36:31 -0500

    modifying radar file names

---
+++ src/hawk/scout/trainer/yolo_radar/yolov5_radar/models/ResnetBackbone.py
@@ -0,0 +1,16 @@
+
+
+
+
+def basicResBlock(x, channel_expansion, strides=(1,1), use_bias=False):
+    pass
+
+
+
+def bottleneckResBlock(x, channel_expansion, strides=(1,1), use_bias=False):
+
+
+
+
+def repeatBlock():
+    
