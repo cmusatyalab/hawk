fa7edbf73dc77246122241fb1c627d334af5f797
Jan Harkes <jaharkes@cs.cmu.edu>
2024-02-26 13:04:15 -0500

    Add 'check' action to hawk_deploy

--- src/hawk/deploy/deploy.py
+++ src/hawk/deploy/deploy.py
@@ -12,6 +12,10 @@ from invoke.exceptions import UnexpectedExit
 from ..deploy_config import DeployConfig
 
 
+def _session_name(config: DeployConfig) -> str:
+    return f"hawk_scout_{config.a2s_port}"
+
+
 def _connect(config: DeployConfig) -> ThreadingGroup:
     return ThreadingGroup(*[str(scout) for scout in config.scouts])
 
@@ -22,7 +26,7 @@ def _start_hawk_scout(conn: ThreadingGroup, config: DeployConfig) -> None:
             "tmux",
             "new-session",
             "-s",
-            f"hawk_scout_{config.a2s_port}",
+            _session_name(config),
             "-d",
             "hawk-venv/bin/hawk_scout",
             "--a2s-port",
@@ -33,15 +37,40 @@ def _start_hawk_scout(conn: ThreadingGroup, config: DeployConfig) -> None:
 
 
 def _stop_hawk_scout(conn: ThreadingGroup, config: DeployConfig) -> None:
-    cmd = shlex.join(
-        [
-            "tmux",
-            "kill-session",
-            "-t",
-            f"hawk_scout_{config.a2s_port}",
-        ],
-    )
-    conn.run(cmd, hide="both", echo=True, warn=True)
+    try:
+        cmd = shlex.join(
+            [
+                "tmux",
+                "kill-session",
+                "-t",
+                _session_name(config),
+            ],
+        )
+        conn.run(cmd, hide="both", echo=True, warn=True)
+    except (GroupException, UnexpectedExit):
+        pass
+
+
+def _check_hawk_scout(conn: ThreadingGroup, config: DeployConfig) -> int:
+    try:
+        cmd = shlex.join(
+            [
+                "tmux",
+                "has-session",
+                "-t",
+                _session_name(config),
+            ],
+        )
+        conn.run(cmd, hide="both", echo=True)
+        return 0
+    except (GroupException, UnexpectedExit):
+        return 1
+
+
+def check_deployment(config: DeployConfig) -> int:
+    """Check deployed scouts."""
+    with _connect(config) as c:
+        return _check_hawk_scout(c, config)
 
 
 def deploy(
@@ -50,8 +79,6 @@ def deploy(
     dist_requirements: Optional[Path] = None,
 ) -> int:
     with _connect(config) as c:
-        _stop_hawk_scout(c, config)
-
         # make sure venv exists
         cmd = shlex.join(
             [
@@ -65,6 +92,8 @@ def deploy(
 
         # upload wheel
         c.put(dist_wheel)
+
+        _stop_hawk_scout(c, config)
 
         # uninstall
         try:
@@ -97,7 +126,7 @@ def deploy(
             c.put(dist_requirements, "requirements-scout.txt")
             pip_install.append("--constraint=requirements-scout.txt")
 
-        # pip_install.append(f"hawk[scout] @ file://@HOME@/{dist_wheel.name}")
+        # pip_install.append(f"cmuhawk[scout] @ file://@HOME@/{dist_wheel.name}")
         pip_install.append(f"./{dist_wheel.name}[scout]")
 
         cmd = shlex.join(pip_install).replace("@HOME@", "'$HOME'")
@@ -117,14 +146,14 @@ def restart_deployment(config: DeployConfig) -> int:
     with _connect(config) as c:
         _stop_hawk_scout(c, config)
         _start_hawk_scout(c, config)
-    return 0
+        return _check_hawk_scout(c, config)
 
 
 def stop_deployment(config: DeployConfig) -> int:
     """Stop previously deployed scouts."""
     with _connect(config) as c:
         _stop_hawk_scout(c, config)
-    return 0
+        return 1 if _check_hawk_scout(c, config) else 0
 
 
 if __name__ == "__main__":
@@ -133,18 +162,28 @@ if __name__ == "__main__":
     from pathlib import Path
 
     parser = argparse.ArgumentParser()
-    parser.add_argument("action", choices=["deploy", "restart", "stop"])
+    parser.add_argument("action", choices=["check", "deploy", "restart", "stop"])
     parser.add_argument("config", type=Path)
     parser.add_argument("wheel", type=Path)
     args = parser.parse_args()
 
     config = DeployConfig.from_yaml(args.config.read_text())
 
-    if args.action == "deploy":
+    if args.action == "check":
+        ret = check_deployment(config)
+        if ret:
+            print("Not all scouts are deployed")
+    elif args.action == "deploy":
         ret = deploy(config, args.wheel)
+        if ret:
+            print("Deployment failed")
     elif args.action == "restart":
         ret = restart_deployment(config)
+        if ret:
+            print("Failed to restart scouts")
     else:
         ret = stop_deployment(config)
+        if ret:
+            print("Failed to stop scouts")
 
     sys.exit(ret)
