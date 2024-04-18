5561b8f384c2bd91f263cf621ac805f1a6e9994b
Jan Harkes <jaharkes@cs.cmu.edu>
2024-04-18 12:29:25 -0400

    Split building wheel and requirements to `hawk_deploy build`
    - Improve hawk_deploy command handling
    - Allow running build/deploy from any subdirectory of the build tree

--- src/hawk/deploy/__main__.py
+++ src/hawk/deploy/__main__.py
@@ -1,47 +1,75 @@
 # SPDX-FileCopyrightText: 2023,2024 Carnegie Mellon University
 # SPDX-License-Identifier: GPL-2.0-only
 
+from __future__ import annotations
+
 import argparse
 import sys
 from pathlib import Path
 
 from ..deploy_config import DeployConfig
-from .build import poetry_build, poetry_export_requirements
-from .deploy import deploy, restart_deployment, stop_deployment
+from .build import builder
+from .deploy import check_deployment, deploy, restart_deployment, stop_deployment
 
 
 def main() -> int:
     parser = argparse.ArgumentParser()
-    parser.add_argument("action", choices=["deploy", "restart", "stop"])
-    parser.add_argument("config", type=Path)
+    subparsers = parser.add_subparsers(required=True, dest="action")
+
+    def build_wrapper(_config: DeployConfig) -> int:
+        dist_wheel, dist_requirements = builder()
+        print("built wheel and requirements files")
+        print(f"{dist_wheel}\n{dist_requirements}")
+        return 0
+
+    build_parser = subparsers.add_parser(
+        "build", help="Build cmuhawk wheel and requirements files"
+    )
+    build_parser.set_defaults(func=build_wrapper)
+
+    def deploy_wrapper(config: DeployConfig) -> int:
+        dist_wheel, dist_requirements = builder()
+        print(f"deploying hawk-scout @ {config.scouts}")
+        return deploy(config, dist_wheel, dist_requirements)
+
+    deploy_parser = subparsers.add_parser("deploy", help="Deploy new Hawk scouts")
+    deploy_parser.add_argument("config", type=Path)
+    deploy_parser.set_defaults(func=deploy_wrapper)
+
+    def check_wrapper(config: DeployConfig) -> int:
+        ret = check_deployment(config)
+        if ret:
+            print("Not all deployed scouts are running")
+        return ret
+
+    check_parser = subparsers.add_parser(
+        "check", help="Check if all deployed Hawk scouts are running"
+    )
+    check_parser.add_argument("config", type=Path)
+    check_parser.set_defaults(func=check_wrapper)
+
+    restart_parser = subparsers.add_parser(
+        "restart", help="Restart deployed Hawk scouts"
+    )
+    restart_parser.add_argument("config", type=Path)
+    restart_parser.set_defaults(func=restart_deployment)
+
+    stop_parser = subparsers.add_parser("stop", help="Stop deployed Hawk scouts")
+    stop_parser.add_argument("config", type=Path)
+    stop_parser.set_defaults(func=stop_deployment)
+
     args = parser.parse_args()
+    try:
+        if "config" in args:
+            config = DeployConfig.from_yaml(args.config.read_text())
+            print(f"{args.action}ing hawk-scout @ {config.scouts}")
+        else:
+            config = None
 
-    config = DeployConfig.from_yaml(args.config.read_text())
-
-    if args.action == "restart":
-        # restart remote (scout) containers.
-        print(f"restarting hawk-scout @ {config.scouts}")
-        return restart_deployment(config)
-
-    if args.action == "stop":
-        # stop remote (scout) containers.
-        print(f"stopping hawk-scout @ {config.scouts}")
-        return stop_deployment(config)
-
-    # build Hawk wheel
-    dist_wheel = poetry_build()
-    if not dist_wheel.exists():
-        print(f"Could not find {dist_wheel}")
+        return int(args.func(config))
+    except Exception as e:
+        print(e)
         return 1
-
-    # export requirements-scout.txt
-    dist_requirements = poetry_export_requirements()
-    if not dist_requirements.exists():
-        print(f"Could not find {dist_requirements}")
-        return 1
-
-    print(f"deploying hawk-scout @ {config.scouts}")
-    return deploy(config, dist_wheel, dist_requirements)
 
 
 if __name__ == "__main__":
--- src/hawk/deploy/build.py
+++ src/hawk/deploy/build.py
@@ -1,8 +1,21 @@
 # SPDX-FileCopyrightText: 2023,2024 Carnegie Mellon University
 # SPDX-License-Identifier: GPL-2.0-only
 
+from __future__ import annotations
+
 import subprocess
 from pathlib import Path
+
+BuildError = Exception
+
+
+def project_root() -> Path:
+    for parent in Path("here").absolute().parents:
+        if parent.joinpath("pyproject.toml").exists():
+            return parent
+
+    msg = "Unable to find root of the Hawk source tree"
+    raise Exception(msg)
 
 
 def poetry_build() -> Path:
@@ -19,11 +32,12 @@ def poetry_build() -> Path:
     result = subprocess.run(["poetry", "version"], capture_output=True, text=True)
     project, version = result.stdout.strip().split()
 
-    return Path(f"dist/{project}-{version}-py3-none-any.whl")
+    root = project_root()
+    return root / "dist" / f"{project}-{version}-py3-none-any.whl"
 
 
 def poetry_export_requirements() -> Path:
-    requirements_txt = Path("dist/requirements-scout.txt")
+    requirements_txt = project_root() / "dist" / "requirements-scout.txt"
     subprocess.run(
         [
             "poetry",
@@ -38,6 +52,21 @@ def poetry_export_requirements() -> Path:
         check=True,
     )
     return requirements_txt
+
+
+def builder() -> tuple[Path, Path]:
+    """build Hawk wheel and requirements files"""
+    dist_wheel = poetry_build()
+    if not dist_wheel.exists():
+        msg = f"Could not find {dist_wheel}"
+        raise BuildError(msg)
+
+    dist_requirements = poetry_export_requirements()
+    if not dist_requirements.exists():
+        msg = f"Could not find {dist_requirements}"
+        raise BuildError(msg)
+
+    return dist_wheel, dist_requirements
 
 
 if __name__ == "__main__":
