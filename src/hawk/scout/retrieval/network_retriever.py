0936838e8613c4ca1829fd64ae2260359d12e8c6
Jan Harkes <jaharkes@cs.cmu.edu>
2024-06-27 13:14:20 -0400

    Fix line-too-long flake8 pre-commit error

--- src/hawk/scout/retrieval/network_retriever.py
+++ src/hawk/scout/retrieval/network_retriever.py
@@ -118,7 +118,8 @@ class NetworkRetriever(Retriever):
                     logger.info(f"About to sleep at: {time.time()}")
                     time.sleep(self._timeout - time_passed)
                 time_start = time.time()
-            ## may need to adjust this timeout of 20 to accomodate higher retrieval rates.
+            ## may need to adjust this timeout of 20 to accomodate higher
+            ## retrieval rates.
 
     def server(self) -> None:
         context = zmq.Context()
