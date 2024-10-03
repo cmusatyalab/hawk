f624b3d7e9c16717ea854c4ab4da4f5cb34188e7
Jan Harkes <jaharkes@cs.cmu.edu>
2024-10-03 13:16:06 -0400

    Add type hints to novel_class_discover.py.

--- src/hawk/scout/trainer/novel_class_discover.py
+++ src/hawk/scout/trainer/novel_class_discover.py
@@ -1,17 +1,19 @@
+# SPDX-FileCopyrightText: 2024 Carnegie Mellon University
+#
+# SPDX-License-Identifier: GPL-2.0-only
+
+from __future__ import annotations
+
 import time
+
 from logzero import logger
 
-def clustering_function(file_name):
+
+def clustering_function(file_name: str) -> None:
     while True:
         logger.info("Runnig novel class discovery...")
         time.sleep(5)
 
 
-
-
-
-
-
-def send_samples():
+def send_samples() -> None:
     pass
-
