25cd7a50f237e85a49f07acd039dd47b663ba9f4
Jan Harkes <jaharkes@cs.cmu.edu>
2025-06-03 11:34:24 -0400

    Move legacy retriever helpers to a mixin class.

--- src/hawk/scout/retrieval/retriever.py
+++ src/hawk/scout/retrieval/retriever.py
@@ -192,11 +192,8 @@ class Retriever(RetrieverBase):
             self.queue_length.dec()
             self.dropped_objects.inc()
 
-    def get_objectid(self, timeout: float | None = None) -> ObjectId | None:
-        """Get a single ML ready object identifier.
-
-        Returns None when there was no candidate before the timeout expired.
-        """
+    def _get_objectid(self, timeout: float | None = None) -> ObjectId | None:
+        """Pop a single ML ready object identifier from the result queue."""
         try:
             object_id = self.result_queue.get(timeout=timeout)
             self.queue_length.dec()
@@ -246,7 +243,7 @@ class Retriever(RetrieverBase):
                 if wait_time < 0:
                     break
 
-            object_id = self.get_objectid(timeout=wait_time)
+            object_id = self._get_objectid(timeout=wait_time)
             if object_id is None:
                 break
 
@@ -260,20 +257,11 @@ class Retriever(RetrieverBase):
 
         return (object_ids, hawk_objects)
 
-    def get_ml_data(self, object_id: ObjectId) -> HawkObject | None:
-        """Get ML ready tile for inferencing or training."""
-        object_path = object_id._file_path(self._data_root)
-        if object_path is None:
-            logger.error(f"Unable to get path for {object_id}")
-            return None
-        try:
-            return HawkObject.from_file(object_path)
-        except FileNotFoundError:
-            logger.error(f"Unable to read {object_id}")
-            return None
+
+class ThumbnailImageMixin(RetrieverBase):
+    """Uses Pillow to derive a thumbnail image from the ML ready data."""
 
     def get_oracle_data(self, object_id: ObjectId) -> list[HawkObject]:
-        """Get Oracle ready data to for labeling at home."""
         ml_object = self.get_ml_data(object_id)
         if ml_object is None or not ml_object.media_type.startswith("image/"):
             raise ValueError("Generic get_oracle_data only works for images")
@@ -302,10 +290,34 @@ class Retriever(RetrieverBase):
 
         return [HawkObject(content=content, media_type="image/jpeg")]
 
+
+class LegacyRetrieverMixin(ThumbnailImageMixin):
+    """Get tile and groundtruth based on the legacy ObjectId format."""
+
+    def __init__(self) -> None:
+        super().__init__()
+
+        # TODO: we probably should be more restrictive about where images
+        # are allowed to be retrieved from.
+        # Random retriever sets this to the parent of the INDEXES directory.
+        self._data_root = Path("/")
+
+    def get_ml_data(self, object_id: ObjectId) -> HawkObject | None:
+        """Return ML ready tile for inferencing or training."""
+        object_path = object_id._file_path(self._data_root)
+        if object_path is None:
+            logger.error(f"Unable to get path for {object_id}")
+            return None
+        try:
+            return HawkObject.from_file(object_path)
+        except FileNotFoundError:
+            logger.error(f"Unable to read {object_id}")
+            return None
+
     def get_groundtruth(self, object_id: ObjectId) -> list[BoundingBox]:
-        """Get groundtruth for logging, statistics and scriptlabeler."""
-        # only handles classification groundtruth for now and gets it the wrong
-        # way by assuming it is stashed in the object id.
+        """Return groundtruth for logging, statistics and scriptlabeler."""
+        # only handles classification groundtruth as it assumes it is stashed
+        # in the object id.
         class_name = object_id._groundtruth()
         if class_name is None:
             return []
