61baa2ac023512d440f19935805ded95ff697b5f
Gil Goldman <gilgoldm@users.noreply.github.com>
2025-06-04 06:22:49 -0400

    support index files generation (#14)

--- src/hawk/scout/retrieval/kinetics600/kinetics_600_retriever.py
+++ src/hawk/scout/retrieval/kinetics600/kinetics_600_retriever.py
@@ -1,6 +1,8 @@
 import random
-from typing import List, Union, Tuple
+from typing import List, Union, Tuple, Dict
+from pathlib import Path
 
+from pandas import DataFrame
 from torch import Tensor
 
 from src.hawk.scout.retrieval.kinetics600.kinetics_ds import KineticsDs
@@ -23,7 +25,9 @@ class K600Retriever(RetrieverIfc):
                              frame_rate=frame_rate,
                              label_transform=label_transform)
 
-    def object_ids_stream(self):  ## Generator
+    def object_ids_stream(self):
+        ## Generator - note that the videos order would be random
+        ## and different at for each generator returned from this method.
         num_videos = len(self.ds)
         video_indexes = list(range(num_videos))
         random.shuffle(video_indexes)
@@ -63,21 +67,50 @@ class K600Retriever(RetrieverIfc):
             -> Union[List[Tuple[int,int]],Tuple[int,int]]:
         if isinstance(object_ids, int):
             object_id = object_ids
-            _, label = self.ds[object_id]
+            label = self.ds.get_label(object_id)
             return label, object_id
         else:
             labels, indexes = [], []
             for object_id in object_ids:
-                _, label = self.ds[object_id]
+                label = self.ds.get_label(object_id)
                 labels.append(label)
                 indexes.append(object_id)
             return [(label,object_id) for label,object_id in zip(labels,indexes)]
 
+    def __len__(self):
+        return len(self.ds)
+
+    def generate_index_files(self, num_scouts: int, path: str) -> List[DataFrame]:
+        assert num_scouts > 0
+        shard_size = len(self) // num_scouts
+        id_stream = self.object_ids_stream()
+        res: List[DataFrame] = []
+        for shard_id in range(num_scouts-1):
+            scout_index: Dict[int,int] = dict()  # video_id -> label
+            for _ in range(shard_size):
+                video_id = next(id_stream)
+                scout_index[video_id] = self.get_ground_truth(video_id)[0]
+                assert scout_index[video_id] in {0,1}
+            res.append(DataFrame.from_dict(scout_index, orient='index', columns=['label']))
+        scout_index: Dict[int, int] = dict()
+        for video_id in id_stream:
+            scout_index[video_id] = self.get_ground_truth(video_id)[0]
+            assert scout_index[video_id] in {0, 1}
+        res.append(DataFrame.from_dict(scout_index, orient='index', columns=['label']))
+        output_path = Path(path)
+        for shard_id, shard in enumerate(res):
+            shard.to_csv(f'{path}/scout_{shard_id}.csv')
+            shard.to_csv(output_path / f'scout_{shard_id}.csv', index_label='video_id')
+        return res
+
+
+
 if __name__ == '__main__':
     k600_retriever = K600Retriever(root='/home/gil/data/k600',
-                                  frames_per_clip=50,
+                                  frames_per_clip=30,
                                   frame_rate=5,
                                   positive_class_idx=0)
+    k600_retriever.generate_index_files(num_scouts=1, path='./')
     id_stream = k600_retriever.object_ids_stream()
     video_id = next(id_stream)
     video, id = k600_retriever.get_ml_ready_data(video_id)
@@ -87,5 +120,3 @@ if __name__ == '__main__':
         f.write(gif)
     label, id = k600_retriever.get_ground_truth(video_id)
     print(f'label: {label}, id: {id}')
-    # print(ml_ready_data[0].shape)
-    # print(oracle_ready_data[0])
