cd55fb6b34973c4121dea2efb4561211d4b0c7d6
Jan Harkes <jaharkes@cs.cmu.edu>
2025-07-01 00:43:26 -0400

    Add pytest for validating and loading pluggable retrievers.
    Does not actually test if the retriever works, just if it loads with a
    minimal configuration.  k600 and http retriever tests are disabled for
    now until dependencies are resolved.

---
+++ tests/scout/test_retriever.py
@@ -0,0 +1,39 @@
+# Copyright (c) 2025 Carnegie Mellon University
+# SPDX-License-Identifier: MIT
+
+import contextlib
+
+import pytest
+
+from hawk.scout.retrieval.loader import load_retriever
+
+CONFIGS = {
+    "frame": {"index_path": "/example/path/to/index"},
+    #    "http": {"base_url": "http://localhost"},
+    #    "k600": {"root": "/path/to/index"},
+    "network": {
+        "index_path": "/example/path/to/index",
+        "server_host": "localhost",
+        "server_port": 8000,
+        "balance_mode": "locally_constant",
+    },
+    "random": {"index_path": "/example/path/to/index"},
+    "tile": {"index_path": "/example/path/to/index"},
+    "video": {"video_path": "/example/path/to/index"},
+}
+
+
+@pytest.mark.parametrize("retriever", CONFIGS.keys())
+def test_validate_retriever_config(retriever):
+    config = dict(CONFIGS[retriever], mission_id="", data_root="/example")
+    retriever_cls = load_retriever(retriever)
+    retriever_cls.validate_config(config)
+
+
+@pytest.mark.scout
+@pytest.mark.parametrize("retriever", CONFIGS.keys())
+def test_load_retriever(retriever):
+    config = dict(CONFIGS[retriever], mission_id="", data_root="/example")
+    retriever_cls = load_retriever(retriever)
+    with contextlib.suppress(FileNotFoundError):
+        retriever_cls.from_config(config)
