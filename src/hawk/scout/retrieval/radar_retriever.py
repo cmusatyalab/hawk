27c583679d09dccf40a95d69fd8aef8c3cba2403
Jan Harkes <jaharkes@cs.cmu.edu>
2025-07-08 23:25:42 -0400

    Add radar retriever.

---
+++ src/hawk/scout/retrieval/radar_retriever.py
@@ -0,0 +1,72 @@
+# SPDX-FileCopyrightText: 2022-2023 Carnegie Mellon University
+#
+# SPDX-License-Identifier: GPL-2.0-only
+
+from __future__ import annotations
+
+from io import BytesIO
+
+import matplotlib
+import matplotlib.pyplot as plt
+import numpy as np
+from logzero import logger
+from PIL import Image
+
+from ...hawkobject import HawkObject
+from ...objectid import LegacyObjectId, ObjectId
+from ...rusty import unwrap
+from .random_retriever import RandomRetriever, RandomRetrieverConfig
+
+matplotlib.use("agg")
+
+
+class RadarRetriever(RandomRetriever):
+    config_class = RandomRetrieverConfig
+    config: RandomRetrieverConfig
+
+    def get_oracle_data(self, object_id: ObjectId) -> list[HawkObject]:
+        """Create a Range-Doppler heatmap from radar data."""
+        try:
+            legacy_id = LegacyObjectId.from_objectid(object_id)
+            object_path = unwrap(legacy_id.file_path(self.config.data_root))
+            assert object_path.suffix in (".npy", ".npz")
+            data = np.load(object_path, allow_pickle=True)
+        except (AssertionError, FileNotFoundError):
+            logger.error(f"Unable to read {object_id}")
+            return []
+
+        # Create RD map
+        plt.imshow(
+            data.sum(axis=2).transpose(), cmap="viridis", interpolation="nearest"
+        )
+        plt.xticks([0, 16, 32, 48, 63], ["-13", "-6.5", "0", "6.5", "13"], fontsize=8)
+        plt.yticks([0, 64, 128, 192, 255], ["50", "37.5", "25", "12.5", "0"])
+        plt.xlabel("velocity (m/s)")
+        plt.ylabel("range (m)")
+        # plt.title("RD Map")
+        with BytesIO() as rdmap:
+            plt.savefig(rdmap, format="png", bbox_inches="tight")
+            rdmap_content = rdmap.getvalue()
+        plt.close("all")
+
+        oracle_data = [HawkObject(content=rdmap_content, media_type="image/png")]
+
+        # Locate stereo image
+        stereo_path = object_path.parent.parent.joinpath(
+            "canvas", object_path.name
+        ).with_suffix(".png")
+
+        if stereo_path.exists():
+            stereo_image = Image.open(stereo_path).convert("RGB")
+
+            # crop the stereo RGB image from the combined stereo + RD map canvas
+            assert stereo_image.size == (2100, 1500)
+            stereo_image = stereo_image.crop((263, 480, 1003, 1035))
+
+            with BytesIO() as tmpfile:
+                stereo_image.save(tmpfile, format="JPEG", quality=85)
+                content = tmpfile.getvalue()
+
+            oracle_data.append(HawkObject(content=content, media_type="image/jpeg"))
+
+        return oracle_data
--- src/hawk/scout/retrieval/retriever_mixins.py
+++ src/hawk/scout/retrieval/retriever_mixins.py
@@ -6,9 +6,6 @@ from __future__ import annotations
 
 from io import BytesIO
 
-import matplotlib
-import matplotlib.pyplot as plt
-import numpy as np
 from logzero import logger
 from PIL import Image
 
@@ -17,8 +14,6 @@ from ...objectid import LegacyObjectId, ObjectId
 from ...rusty import unwrap
 from ..core.result_provider import BoundingBox
 from .retriever import ImageRetrieverConfig, RetrieverBase
-
-matplotlib.use("agg")
 
 THUMBNAIL_SIZE = 256
 
@@ -97,42 +92,3 @@ class LegacyRetrieverMixin(ThumbnailImageMixin):
                 x=0.5, y=0.5, w=1.0, h=1.0, class_name=class_name, confidence=1.0
             )
         ]
-
-
-class LegacyRadarMixin(LegacyRetrieverMixin):
-    """Derive Oracle data for a radar dataset."""
-
-    def get_oracle_data(self, object_id: ObjectId) -> list[HawkObject]:
-        """Create a Range-Doppler heatmap from radar data."""
-        try:
-            legacy_id = LegacyObjectId.from_objectid(object_id)
-            object_path = unwrap(legacy_id.file_path(self.config.data_root))
-            assert object_path.suffix in (".npy", ".npz")
-            data = np.load(object_path, allow_pickle=True)
-        except (AssertionError, FileNotFoundError):
-            logger.error(f"Unable to read {object_id}")
-            return []
-
-        # Create RD map
-        plt.imshow(
-            data.sum(axis=2).transpose(), cmap="viridis", interpolation="nearest"
-        )
-        plt.xticks([0, 16, 32, 48, 63], ["-13", "-6.5", "0", "6.5", "13"], fontsize=8)
-        plt.yticks([0, 64, 128, 192, 255], ["50", "37.5", "25", "12.5", "0"])
-        plt.xlabel("velocity (m/s)")
-        plt.ylabel("range (m)")
-        # plt.title("RD Map")
-        with BytesIO() as rdmap:
-            plt.savefig(rdmap, format="png", bbox_inches="tight")
-            rdmap_content = rdmap.getvalue()
-        plt.close("all")
-
-        oracle_data = [HawkObject(content=rdmap_content, media_type="image/png")]
-
-        # Locate stereo image
-        object_base = object_path.stem.split("_", 1)[0]
-        stereo_image = object_path.parent / "stereo_left" / f"{object_base}_left.jpg"
-        if stereo_image.exists():
-            oracle_data.append(HawkObject.from_file(stereo_image))
-
-        return oracle_data
