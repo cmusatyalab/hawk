74c01d267b669894750a24161c3cb678d6a79771
Jan Harkes <jaharkes@cs.cmu.edu>
2025-07-11 13:32:24 -0400

    Kinetics retriever updates.
    - don't use index files, use a predictable random shuffle and N of M
      slicing to divide the data among scouts. We will have to inject the N
      and M values when initializing retrievers.
    - add a main entrypoint to kinetics_ds to download and generate
      precomputed metadata.

--- src/hawk/scout/retrieval/kinetics600/kinetics_600_retriever.py
+++ src/hawk/scout/retrieval/kinetics600/kinetics_600_retriever.py
@@ -12,7 +12,6 @@ from typing import Iterator
 
 import torch
 from logzero import logger
-from pandas import DataFrame
 
 from ....classes import POSITIVE_CLASS
 from ....hawkobject import HawkObject
@@ -24,11 +23,15 @@ from .video_utils import create_gif_from_video_tensor_bytes
 
 
 class K600RetrieverConfig(RetrieverConfig):
-    index_path: Path
+    dataset_root: Path
     frames_per_clip: int
     frame_rate: int
     positive_class_idx: int = 0
     timeout: float = 2.0
+
+    # scout 'N of M' (N index starts at 1)
+    N: int = 1
+    M: int = 1
 
 
 # Local class to wrap direct accesses to the index stored in ObjectId
@@ -59,21 +62,26 @@ class K600Retriever(Retriever):
         super().__init__(config)
 
         self.ds = KineticsDs(
-            root=self.config.index_path.parent,
-            video_clips_pkl_name="train.pkl",
+            root=self.config.dataset_root,
             frames_per_clip=self.config.frames_per_clip,
             step_between_clips=self.config.frames_per_clip,
-            split="train",
             frame_rate=self.config.frame_rate,
         )
 
     def _get_next_objectid(self) -> Iterator[K600_ObjectId]:
-        ## Generator - note that the videos order would be random
-        ## and different at for each generator returned from this method.
         num_videos = len(self.ds)
-        video_indexes = [K600_ObjectId.from_idx(n) for n in range(num_videos)]
+        video_indexes = list(range(num_videos))
+
+        # Shuffle the index, but keep the random order predictable
+        random.seed(20220718)
         random.shuffle(video_indexes)
-        yield from video_indexes
+
+        # slice based on the number of scouts
+        if self.config.M > 1:
+            assert self.config.N >= 1
+            video_indexes = video_indexes[self.config.N - 1 : -1 : self.config.M]
+
+        yield from map(K600_ObjectId.from_idx, video_indexes)
 
     def get_next_objectid(self) -> Iterator[K600_ObjectId | None]:
         for object_id in self._get_next_objectid():
@@ -84,7 +92,7 @@ class K600Retriever(Retriever):
 
     def get_ml_data(self, object_id: ObjectId) -> HawkObject:
         index = K600_ObjectId.from_oid(object_id).index
-        video = self.ds[index]
+        video = self.ds.get_video(index)
         with io.BytesIO() as f:
             torch.save(video, f)
             content = f.getvalue()
@@ -92,17 +100,17 @@ class K600Retriever(Retriever):
 
     def get_oracle_data(self, object_id: ObjectId) -> list[HawkObject]:
         index = K600_ObjectId.from_oid(object_id).index
-        video = self.ds[index]
+        video = self.ds.get_video(index)
         gif = create_gif_from_video_tensor_bytes(video)
         return [HawkObject(content=gif, media_type="image/gif")]
 
-    def is_groundtruth_positive(self, object_id: ObjectId) -> bool:
+    def _is_groundtruth_positive(self, object_id: ObjectId) -> bool:
         index = K600_ObjectId.from_oid(object_id).index
         label = self.ds.get_label(index)
         return label == self.config.positive_class_idx
 
     def get_groundtruth(self, object_id: ObjectId) -> list[BoundingBox]:
-        if not self.is_groundtruth_positive(object_id):
+        if not self._is_groundtruth_positive(object_id):
             return []
         return [
             BoundingBox(
@@ -113,51 +121,33 @@ class K600Retriever(Retriever):
     def __len__(self) -> int:
         return len(self.ds)
 
-    def generate_index_files(self, num_scouts: int, path: str) -> list[DataFrame]:
-        assert num_scouts > 0
-        shard_size = len(self) // num_scouts
-        id_stream = self._get_next_objectid()
-        res: list[DataFrame] = []
-        for _shard_id in range(num_scouts - 1):
-            scout_index: dict[int, int] = dict()  # video_id.index -> label
-            for _ in range(shard_size):
-                video_id = next(id_stream)
-                scout_index[video_id.index] = int(
-                    self.is_groundtruth_positive(video_id)
-                )
-                assert scout_index[video_id.index] in {0, 1}
-            res.append(
-                DataFrame.from_dict(scout_index, orient="index", columns=["label"])
-            )
-        scout_index = dict()
-        for video_id in id_stream:
-            scout_index[video_id.index] = int(self.is_groundtruth_positive(video_id))
-            assert scout_index[video_id.index] in {0, 1}
-        res.append(DataFrame.from_dict(scout_index, orient="index", columns=["label"]))
-        output_path = Path(path)
-        for shard_id, shard in enumerate(res):
-            shard.to_csv(f"{path}/scout_{shard_id}.csv")
-            shard.to_csv(output_path / f"scout_{shard_id}.csv", index_label="video_id")
-        return res
-
 
 if __name__ == "__main__":
+    import argparse
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("dataset_root", type=Path)
+    args = parser.parse_args()
+
     k600_retriever = K600Retriever.from_config(
         dict(
-            index_path="/home/gil/data/k600/index.csv",
+            dataset_root=args.dataset_root,
             frames_per_clip=30,
             frame_rate=5,
             positive_class_idx=0,
         )
     )
-    k600_retriever.generate_index_files(num_scouts=1, path="./")
-    id_stream = k600_retriever._get_next_objectid()
+
+    id_stream = k600_retriever.get_next_objectid()
     video_id = next(id_stream)
+    assert video_id is not None
+
     ml_data = k600_retriever.get_ml_data(video_id)
-    oracle_data = k600_retriever.get_oracle_data(video_id)[0]
     assert ml_data.media_type == "x-tensor/pytorch"
+
+    oracle_data = k600_retriever.get_oracle_data(video_id)[0]
     assert oracle_data.media_type == "image/gif"
     oracle_data.to_file("in_memory_output")
 
-    label = int(k600_retriever.is_groundtruth_positive(video_id))
+    label = int(k600_retriever._is_groundtruth_positive(video_id))
     print(f"label: {label}, id: {video_id}")
--- src/hawk/scout/retrieval/kinetics600/kinetics_ds.py
+++ src/hawk/scout/retrieval/kinetics600/kinetics_ds.py
@@ -12,33 +12,33 @@ from torchvision.datasets import Kinetics
 
 
 class KineticsDs(Kinetics):  # type: ignore[misc]
+    _metadata_path = Path("video_clips_cache", "train.pkl")
+
     def __init__(
         self,
         root: Path,
         frames_per_clip: int,
-        video_clips_pkl_name: str,
-        split: str,
         frame_rate: int | None = None,
         step_between_clips: int = 1,
         transform: Callable[[Tensor], Tensor] | None = None,
     ) -> None:
-        video_clips_pkl_path = root / "video_clips_cache" / video_clips_pkl_name
-        with open(video_clips_pkl_path, "rb") as file:
+        metadata_cache = root / self._metadata_path
+        with metadata_cache.open("rb") as file:
             _precomputed_metadata: dict[str, Any] | None = pickle.load(file)
 
         super().__init__(
-            root=str(root),
+            root=root,
             frames_per_clip=frames_per_clip,
             num_classes="600",
-            split=split,
+            split="train",
             frame_rate=frame_rate,
             step_between_clips=step_between_clips,
             transform=transform,
             _precomputed_metadata=_precomputed_metadata,
         )
 
-    def __getitem__(self, idx: int) -> Tensor:
-        video = self.video_clips.get_clip(idx)[0]
+    def get_video(self, idx: int) -> Tensor:
+        video, _audio, _info, _index = self.video_clips.get_clip(idx)
         if self.transform is not None:
             video = self.transform(video)
         return video
@@ -49,7 +49,37 @@ class KineticsDs(Kinetics):  # type: ignore[misc]
             raise IndexError(f"Index {idx} out of range ({nclips} number of clips)")
 
         video_idx, clip_idx = self.video_clips.get_clip_location(idx)
-        # video_path = self.video_clips.video_paths[video_idx]
-
         label: int = self.samples[video_idx][1]
         return label
+
+    @classmethod
+    def download(cls, root: Path, num_download_workers: int = 1) -> None:
+        print("Downloading Kinetics600 dataset to {root}.")
+
+        ds = Kinetics(
+            root=root,
+            frames_per_clip=30,
+            num_classes="600",
+            split="train",
+            download=True,
+            num_download_workers=num_download_workers,
+        )
+
+        metadata_cache = root / cls._metadata_path
+        with metadata_cache.open("wb") as file:
+            pickle.dump(ds.video_clips.metadata, file)
+
+        print("Kinetics600 dataset downloaded to {root}.")
+
+
+if __name__ == "__main__":
+    import argparse
+
+    parser = argparse.ArgumentParser(description="Download Kinetics-600 dataset")
+    parser.add_argument(
+        "--num-workers", type=int, default=1, help="Number of download workers"
+    )
+    parser.add_argument("root", type=Path, help="Root directory")
+    args = parser.parse_args()
+
+    KineticsDs.download(args.root, num_download_workers=args.num_workers)
